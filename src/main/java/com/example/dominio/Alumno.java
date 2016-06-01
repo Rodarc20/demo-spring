488a6260c31b863e624820de4155a14b9a9ff5a2
Rodrigo Gutierrez <rodrigo.gutierrez.k@gmail.com>
2016-06-01 18:51:23 -0500

    new branch

--- src/main/java/com/example/dominio/Alumno.java
+++ src/main/java/com/example/dominio/Alumno.java
@@ -2,12 +2,17 @@ package com.example.dominio;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.SequenceGenerator;
 /**
  * Created by rodrigo on 20/05/16.
  */
 @Entity
 public class Alumno {
     @Id
+    @SequenceGenerator(name="Alumno_ID_GENERATOR", sequenceName="Alumno_ID_SEQ")
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="Alumno_ID_GENERATOR")
     public Integer id;
     public String nombres;
     public String apellidoPaterno;
@@ -17,8 +22,7 @@ public class Alumno {
     public Alumno() {
     }
 
-    public Alumno(String nombres, String apellidoPaterno,
-                  Integer promedio) {
+    public Alumno(String nombres, String apellidoPaterno, Integer promedio) {
         this.nombres = nombres;
         this.apellidoPaterno = apellidoPaterno;
         this.promedio = promedio;
--- src/main/java/com/example/repositorio/AlumnoRepositorio.java
+++ src/main/java/com/example/repositorio/AlumnoRepositorio.java
@@ -3,20 +3,10 @@ package com.example.repositorio;
 /**
  * Created by rodrigo on 20/05/16.
  */
-
-import java.util.Arrays;
 import java.util.List;
-
+import org.springframework.data.repository.CrudRepository;
 import com.example.dominio.Alumno;
 
-public class AlumnoRepositorio {
-    Alumno[] data = {
-            new Alumno("Juan", "Perez", 16),
-            new Alumno("Maria", "Castro", 14),
-            new Alumno("Jose", "Ruiz", 18),
-    };
-
-    public List<Alumno> listar() {
-        return Arrays.asList(data);
-    }
+public interface AlumnoRepositorio extends CrudRepository <Alumno, Integer>{
+    List<Alumno> findAll();
 }
