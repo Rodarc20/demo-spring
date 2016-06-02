544a5fd87d0beca466bb55260df39319ee410d1f
Rodrigo Gutierrez <rodrigo.gutierrez.k@gmail.com>
2016-06-01 19:02:34 -0500

    modificaion a Demo2Application.java

--- src/main/java/com/example/Demo2Application.java
+++ src/main/java/com/example/Demo2Application.java
@@ -8,8 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.example.dominio.Alumno;
 import com.example.repositorio.AlumnoRepositorio;
@@ -19,8 +19,8 @@ import com.example.repositorio.AlumnoRepositorio;
 @EntityScan(basePackageClasses = Alumno.class)
 @SpringBootApplication
 public class Demo2Application {
-
-    private AlumnoRepositorio repositorio = new AlumnoRepositorio();
+    @Autowired
+    AlumnoRepositorio repositorio;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo2Application.class, args);
@@ -28,45 +28,8 @@ public class Demo2Application {
 
     @RequestMapping("/alumnos")
     @ResponseBody
-    public List<Alumno> alumnos(String ordenarPor) {
-        List<Alumno> a = repositorio.listar();
-
-        if(ordenarPor.compareTo("nombre")==0) {
-            int ini = 0;
-            int fin = a.size() - 1;
-            while (fin > ini) {
-                for (int i = 0; i < fin; i++) {
-                    if (a.get(i).nombres.compareTo(a.get(i + 1).nombres) > 0) { //if(a.get(i).nombres > a.get(i+1).nombres){
-                        a.set(i + 1, a.set(i, a.get(i + 1)));
-                    }
-                }
-                fin--;
-            }
-        }
-        if(ordenarPor.compareTo("apellidoP")==0) {
-            int ini = 0;
-            int fin = a.size() - 1;
-            while (fin > ini) {
-                for (int i = 0; i < fin; i++) {
-                    if (a.get(i).apellidoPaterno.compareTo(a.get(i + 1).apellidoPaterno) > 0) { //if(a.get(i).nombres > a.get(i+1).nombres){
-                        a.set(i + 1, a.set(i, a.get(i + 1)));
-                    }
-                }
-                fin--;
-            }
-        }
-        if(ordenarPor.compareTo("promedio")==0) {
-            int ini = 0;
-            int fin = a.size() - 1;
-            while (fin > ini) {
-                for (int i = 0; i < fin; i++) {
-                    if (a.get(i).promedio.compareTo(a.get(i + 1).promedio) > 0) { //if(a.get(i).nombres > a.get(i+1).nombres){
-                        a.set(i + 1, a.set(i, a.get(i + 1)));
-                    }
-                }
-                fin--;
-            }
-        }
-        return a;
+    public List<Alumno> alumnos() {
+        repositorio.save(new Alumno("Juan", "Perez", 10));
+        return repositorio.findAll();
     }
 }
