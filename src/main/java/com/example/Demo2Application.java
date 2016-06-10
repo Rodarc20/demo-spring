d52532d8d3e5ccafd6d4db30dd759d54b540e5fe
Rodrigo Gutierrez <rodrigo.gutierrez.k@gmail.com>
2016-06-10 18:35:22 -0500

    Agregado la parte de curso, igualado con el repositorio del profesor

--- src/main/java/com/example/Demo2Application.java
+++ src/main/java/com/example/Demo2Application.java
@@ -1,35 +1,18 @@
 package com.example;
 
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import com.example.dominio.Alumno;
-import com.example.repositorio.AlumnoRepositorio;
 
 @EnableAutoConfiguration
-@Controller
 @EntityScan(basePackageClasses = Alumno.class)
 @SpringBootApplication
 public class Demo2Application {
-    @Autowired
-    AlumnoRepositorio repositorio;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo2Application.class, args);
 	}
-
-    @RequestMapping("/alumnos")
-    @ResponseBody
-    public List<Alumno> alumnos() {
-        repositorio.save(new Alumno("Juan", "Perez", 10));
-        return repositorio.findAll();
-    }
 }
---
+++ src/main/java/com/example/repositorio/CursoRepositorio.java
@@ -0,0 +1,15 @@
+/**
+ * Created by rodrigo on 10/06/16.
+ */
+package com.example.repositorio;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.dominio.Curso;
+
+public interface CursoRepositorio extends CrudRepository<Curso, String> {
+	List<Curso> findAll();
+	Curso findByCodigo(String codigo);
+}
